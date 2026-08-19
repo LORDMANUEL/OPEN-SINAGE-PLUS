540f88b79784408ca4a06a583177ba3d0a06137e
LORDMANUEL <43154524+LORDMANUEL@users.noreply.github.com>
2026-08-18 21:36:12 -0600

    feat: wire Studio, Media and browser player routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import TopBar from './components/TopBar';
 import DashboardView from './views/DashboardView';
 import ScreenListView from './views/ScreenListView';
 import IntegrationView from './views/IntegrationView';
+import StudioView from './views/StudioView';
+import MediaView from './views/MediaView';
+import PlayerScreen from './views/PlayerScreen';
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -17,6 +20,8 @@ function App() {
   const renderContent = () => {
     switch (currentView) {
       case 'dashboard': return <DashboardView />;
+      case 'studio': return <StudioView />;
+      case 'media': return <MediaView />;
       case 'signage': return <ScreenListView type="signage" title="Pantallas Digital Signage" />;
       case 'kiosk': return <ScreenListView type="kiosk" title="Kioscos Interactivos" />;
       case 'dashboards': return <ScreenListView type="dashboard" title="Dashboards BI" />;
@@ -37,6 +42,13 @@ function App() {
   );
 }
 
+function getPlayerTokenFromPath() {
+  const match = window.location.pathname.match(/^\/player\/([a-zA-Z0-9_-]{12,128})\/?$/);
+  return match?.[1] ?? null;
+}
+
 export default function AppWrapper() {
+  const playerToken = getPlayerTokenFromPath();
+  if (playerToken) return <PlayerScreen token={playerToken} />;
   return <AppProvider><App /></AppProvider>;
 }
